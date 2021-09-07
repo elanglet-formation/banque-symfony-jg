82f495c5512fda5eeeae1ee6895003db731486e8
Joseph Guézel <joseph.guezel@assurance-maladie.fr>
2021-09-07 11:03:14 +0200

    Ajoute des tests (test1, test2) et setUp/tearDwn

--- tests/ExempleTest.php
+++ tests/ExempleTest.php
@@ -6,8 +6,31 @@ use PHPUnit\Framework\TestCase;
 
 class ExempleTest extends TestCase
 {
-    public function testSomething(): void
+    public static function setUpBeforeClass():void{
+        echo "\nAvant tous les tests";
+    }
+    
+    public static function tearDownAfterClass():void{
+        echo "\nAprès tous les tests";
+    }
+    
+    public function setUp():void{
+        echo "\nAvant chaque test";
+    }
+    
+    public function tearDown():void{
+        echo "\nAprès chaque test";
+    }
+    
+    public function test1(): void
     {
+        echo "\nTest1";
+        $this->assertTrue(true);
+    }
+    
+    public function test2(): void
+    {
+        echo "\nTest2";
         $this->assertTrue(true);
     }
 }
